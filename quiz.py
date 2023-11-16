6bdc99c6e754286187d07d0720ec5ed910be3b63
Lucas Vahey <242065lv@internal.salesianum.org>
2023-11-16 14:54:33 -0500

    made code work

--- quiz.py
+++ quiz.py
@@ -2,22 +2,14 @@
 import random
 def main():
     play_game()
-    genRandomNumber()
-    playerGuess()
-    give_feedback()
+ 
+
     
-def genRandomNumber():
-    secretNumber = random.randint(1, 100)
-    
-   print("try the guessing game")
-   print("I have selected a number between 1 and 100, try to guess it")
-   if guessed_number == secretnumber:
 def play_game():
-    secretnumber=random.randint(1,100)
-    attempts=0
-    guessed_number = int(input("enter your guess"))
-    
-def give_feedback():
+    secretNumber = random.randint(1, 100)
+    print("try the guessing game")
+    print("I have selected a number between 1 and 100, try to guess it")
+    guess = input("Please enter a guess of a number between 1 and 100: ")
     if(int(guess) > secretNumber):
         print("Too high")
     elif(int(guess) < secretNumber):
@@ -25,6 +17,7 @@ def give_feedback():
     else:
         print("Correct")
         print("1 attempt")
+    guess1 = input("Please enter a guess of a number between 1 and 100: ")
     if(int(guess1) > secretNumber):
         print("Too high")
     elif(int(guess1) < secretNumber):
@@ -32,19 +25,14 @@ def give_feedback():
     else:
         print("Correct")
         print("2 attempts")
+    guess2 = input("Please enter a guess of a number between 1 and 100: ")
     if(int(guess2) == secretNumber):
         print("Correct")
         print("3 attempts")
     else:
         print("The number is " + str(secretNumber))
         print("You didn't correctly guess the number.")
-        
-def playerGuess():
-    guess = input("Please enter a guess of a number between 1 and 100: ")
-    guess1 = input("Please enter a guess of a number between 1 and 100: ") 
-    guess2 = input("Please enter a guess of a number between 1 and 100: ") 
-    
-
+         
 if __name__ == "__main__":
     main()
 
